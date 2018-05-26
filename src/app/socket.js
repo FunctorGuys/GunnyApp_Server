5e171511c547285400d7ca173c1fa76783ae8e38
jinsphan <pvtin1996@gmail.com>
2018-05-26 23:53:51 +0700

    Socket

--- src/app/socket.js
+++ src/app/socket.js
@@ -1,7 +1,7 @@
 var socketio = require('socket.io');
 
 const clients = {};
-const rooms = [
+let rooms = [
     {
         id: 154832146284,
         name: "ROOM001",
@@ -37,20 +37,6 @@ const rooms = [
         },
         invitee: {}
     },
-    {
-        id: 154855212146284,
-        name: "ROOM003",
-        isFull: false,
-        creater: {
-            id: 12541215,
-            username: "user005",
-            fullname: "User 005",
-            isReady: false,
-            win: 21,
-            lose: 12,
-        },
-        invitee: {}
-    },
 ];
 
 
@@ -64,13 +50,105 @@ const connectSocket = (server) => {
 }
 
 const listenClient = socketClient => {
+    // Client get all rooms
     socketClient.on("getActiveRooms", () => {
         socketClient.emit("receivedActiveRooms", rooms);
     })
+
+    // Client enter a room
+    socketClient.on("enterRoom", ({room_id, user}) => {
+        // console.log("Client enter room", room_id, rooms);
+
+        let roomChange = {};
+        rooms = rooms.map(room => {
+            if (room.id === room_id) {
+                return roomChange = {
+                    ...room,
+                    isFull: true,
+                    invitee: {
+                        ...user,
+                        isReady: false
+                    }
+                }
+            }
+            return room;
+        })
+        updateRoom(roomChange);
+        
+    })
+
+    // Client on leaveRoom
+    socketClient.on("leaveRoom", room_id => {
+        let roomLeave = rooms.filter(room => room.id === room_id)[0];
+        console.log("Client leave room", room_id);
+        let roomChange = {};
+        if (roomLeave.isFull) {
+            rooms = rooms.map(room => {
+                if (room.id === room_id) {
+                    return roomChange = {
+                        ...room,
+                        isFull: false,
+                        invitee: {},
+                    }
+                }
+                return room;
+            })
+
+            updateRoom(roomChange);
+        } else {
+            cancelRoom(roomLeave.id);
+        }
+    })
+
+    // Client create new room 
+    socketClient.on("createRoom", newRoom => {
+        console.log("Client create new room");
+        rooms.push(newRoom);
+        createRoom(newRoom, () => {
+            socketClient.emit("createdRoom", newRoom);
+        });
+    })
+
+
+    // Client disconnect
     socketClient.on("disconnect", () => {
         console.log("disconnect", socketClient.id);
         delete clients[socketClient.id];
     })
+}
+
+const updateRoom = room => {
+    Object.keys(clients).forEach(client_id => {
+        const client = clients[client_id];
+        if (client.id) {
+            client.emit("updateRoom", room);
+        }
+    });
+}
+
+const cancelRoom = room_id => {
+    rooms = rooms.filter(room => room.id !== room_id);
+    Object.keys(clients).forEach(client_id => {
+        const client = clients[client_id];
+        if (client.id) {
+            client.emit("cancelRoom", room_id);
+        }
+    });
+}
+
+const createRoom = async (room, cb) => {
+    try {
+        await Object.keys(clients).forEach(client_id => {
+            const client = clients[client_id];
+            if (client.id) {
+                client.emit("createRoom", room);
+            }
+        });
+        cb();
+
+    } catch(er) {
+        console.log(er);
+    }
 }
 
 module.exports = {
